e05993052e9d169ce44b74d6694fc7da37996d55
foximaychik <128075298+foximaychik@users.noreply.github.com>
2024-01-10 18:52:57 +0400

    Add files via upload

---
+++ src/net/foxi/samples/Sample.java
@@ -0,0 +1,9 @@
+package net.foxi.samples;
+
+import net.foxi.Colors;
+
+public class Sample {
+    public static void main(String[] args) {
+        Colors.Test(); //checks if your console supports colors
+    }
+}
